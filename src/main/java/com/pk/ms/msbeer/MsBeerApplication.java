2721d2c303ee427c09e276f762498abb8e362fbc
Peter Kang <kangguolong@gmail.com>
2020-10-01 23:41:04 +1300

    Project Architecture

---
+++ src/main/java/com/pk/ms/msbeer/MsBeerApplication.java
@@ -0,0 +1,13 @@
+package com.pk.ms.msbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBeerApplication.class, args);
+    }
+
+}
